499a724a583dcb0a240f341836a8dee5a11ad837
Taeyoung Kwon <100225783+teyeong@users.noreply.github.com>
2024-02-24 00:11:45 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int N, M;
+int arr[9];
+bool visited[9];
+
+void dfs(int cnt) {
+    if (cnt == M) {
+        for (int i = 0; i < M; i++) {
+            cout << arr[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+    for (int j = 1; j <= N; j++) {
+        if (!visited[j] && j > arr[cnt - 1]) {
+            visited[j] = 1;
+            arr[cnt] = j;
+            dfs(cnt + 1);
+            visited[j] = false;
+        }
+    }
+}
+
+int main () {
+    cin >> N >> M;
+    dfs(0);
+    return 0;
+}
