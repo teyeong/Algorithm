296a274435d58d4f4623a0dd1b229ccba387c4d9
Taeyoung Kwon <100225783+teyeong@users.noreply.github.com>
2024-04-27 11:32:24 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.39 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -0,0 +1,48 @@
+import re
+
+def solution(today, terms, privacies):
+    answer = []
+    #terms_list = [[0] * 2 for k in range(len(terms))]
+    terms_alpha = []
+    terms_dura = []
+    today_list = list(map(int, re.split('[.]', today)))
+    
+    for j in range(len(terms)):
+        alphabet, duration = re.split('[ ]', terms[j])
+        #terms_list[j][0] = alphabet
+        #terms_list[j][1] = int(duration)
+        terms_alpha.append(alphabet)
+        terms_dura.append(int(duration))
+    
+    for i in range(len(privacies)):
+        Y, M, D, term = re.split('[ .]', privacies[i])
+        term_index = terms_alpha.index(term)
+        
+        plus_year = 0
+        plus_month = terms_dura[term_index]
+        if (plus_month > 11):
+            plus_year = plus_month // 12
+            plus_month %= 12
+        
+        result_year = int(Y) + plus_year
+        result_month = int(M) + plus_month
+        result_day = int(D)
+        
+        if (result_month > 12):
+            result_year += 1
+            result_month -= 12
+        
+        if (result_year < today_list[0]):
+            answer.append(i + 1)
+            continue
+        
+        
+        if (result_year == today_list[0]):
+            if (result_month < today_list[1]):
+                answer.append(i + 1)
+            elif (result_month == today_list[1]):
+                if (result_day <= today_list[2]):
+                    answer.append(i + 1)
+        
+        
+    return answer
