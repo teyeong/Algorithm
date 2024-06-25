7a7568a4fe04f881deddba54fadf2fd60ebc9ad9
Taeyoung Kwon <100225783+teyeong@users.noreply.github.com>
2024-06-25 23:47:23 +0900

    [Silver V] Title: 금메달, 은메달, 동메달은 누가?, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/3230. 금메달， 은메달， 동메달은 누가？/금메달， 은메달， 동메달은 누가？.py
@@ -0,0 +1,16 @@
+N, M = map(int, input().split())
+race1 = []
+race2 = []
+player1 = [i for i in range(1, N + 1)] # 첫 번째 경주에 참여하는 선수 목록
+for j in range(N):
+    rank = int(input())
+    race1.insert(rank - 1, j + 1)
+
+player2 = race1[:M] # 두 번째 경주에 참여하는 선수 목록
+for _ in range(M):
+    rank = int(input())
+    player = player2.pop()
+    race2.insert(rank - 1, player)
+
+for k in range(3):
+    print(race2[k])
