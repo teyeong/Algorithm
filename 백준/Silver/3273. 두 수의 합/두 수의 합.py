615e1eb6cefad60085c5245fc52a44332a442f8c
Taeyoung Kwon <100225783+teyeong@users.noreply.github.com>
2024-06-27 23:33:04 +0900

    [Silver III] Title: 두 수의 합, Time: 84 ms, Memory: 42036 KB -BaekjoonHub

---
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.py
@@ -0,0 +1,19 @@
+n = int(input())
+arr = list(map(int, input().split()))
+x = int(input())
+cnt = 0
+arr.sort()
+arr = [k for k in arr if k < x]
+start, end = 0, len(arr) - 1
+while start < end:
+    mid = (start + end) // 2
+    temp = arr[start] + arr[end]
+    if x == temp:
+        cnt += 1
+        start += 1
+        end -= 1
+    elif x < temp:
+        end -= 1
+    else:
+        start += 1
+print(cnt)
