877c951d3e4cb610bf585f140fa638ac266fdb4b
Taeyoung Kwon <100225783+teyeong@users.noreply.github.com>
2024-09-02 23:48:02 +0900

    [Silver V] Title: D-Day, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1308. D－Day/D－Day.py
+++ 백준/Silver/1308. D－Day/D－Day.py
@@ -1,15 +1,48 @@
-from datetime import datetime
-
 today = list(map(int, input().split()))
 dday = list(map(int, input().split()))
 
-limit_day = datetime(today[0] + 1000, today[1], today[2])
-today = datetime(today[0], today[1], today[2])
-dday = datetime(dday[0], dday[1], dday[2])
+ans = 0
 
-diff = dday - today
+# 윤년 계산
+leap_year = []
+for i in range(today[0], dday[0] + 1):
+    if not i % 4:
+        if i % 100:
+            leap_year.append(i)
+        elif not i % 400:
+            leap_year.append(i)
 
-if diff >= limit_day - today:
-    print("gg")
+# 연도 계산
+ans += (dday[0] - today[0] + 1) * 365 + len(leap_year)
+
+# 달 계산
+month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+for i in range(1, today[1]):
+    if today[0] in leap_year:
+        if i == 2:
+            ans -= 29
+        else:
+            ans -= month[i - 1]
+    else:
+        ans -= month[i - 1]
+
+ans -= today[2]
+
+for i in range(dday[1] + 1, 13):
+    if dday[0] in leap_year:
+        if i == 2:
+            ans -= 29
+        else:
+            ans -= month[i - 1]
+    else:
+        ans -= month[i - 1]
+
+if dday[0] in leap_year:
+    if dday[1] == 2:
+        ans -= 29 - dday[2]
+    else:
+        ans -= month[dday[1] - 1] - dday[2]
 else:
-    print(f"D-{diff.days}")
+    ans -= month[dday[1] - 1] - dday[2]
+
+print(f'D-{ans}' if ans <= 365242 else 'gg')
