953ad9d3fad23ab316e7de105facf8691677d898
Taeyoung Kwon <100225783+teyeong@users.noreply.github.com>
2024-09-06 01:19:04 +0900

    [Gold I] Title: 최솟값, Time: 1492 ms, Memory: 39356 KB -BaekjoonHub

---
+++ 백준/Gold/10868. 최솟값/최솟값.py
@@ -0,0 +1,50 @@
+import math
+import sys
+
+input = sys.stdin.readline
+sys.setrecursionlimit(10 ** 8)
+
+def make_tree(idx, s, e):
+    if s == e:
+        tree[idx] = arr[s]
+        return tree[idx]
+    
+    mid = (s + e) // 2
+    
+    l = make_tree(idx * 2, s, mid)
+    r = make_tree(idx * 2 + 1, mid + 1, e)
+    
+    tree[idx] = min(l, r)
+    return tree[idx]
+
+def search(s, e, idx):
+    if e < a or b < s:
+        return 10 ** 9 + 1
+    
+    if a <= s and e <= b:
+        return tree[idx]
+    
+    mid = (s + e) // 2
+
+    l = search(s, mid, idx * 2)
+    r = search(mid + 1, e, idx * 2 + 1)
+
+    return min(l, r)
+
+N, M = map(int, input().split())
+
+arr = []
+
+for _ in range(N):
+    arr.append(int(input()))
+
+b = math.ceil(math.log2(N)) + 1
+node = 1 << b
+tree = [0 for _ in range(node)]
+make_tree(1, 0, len(arr) - 1)
+
+for _ in range(M):
+    a, b = map(int, input().split())
+    a, b = a - 1, b - 1
+    ans = search(0, len(arr) - 1, 1)
+    print(ans)
