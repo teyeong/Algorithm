adeeacd6dce2a8825f3e62a82faa1a3838236b07
Taeyoung Kwon <100225783+teyeong@users.noreply.github.com>
2025-01-03 14:25:47 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 112.44 ms, Memory: 27.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -0,0 +1,10 @@
+def solution(arr):
+    answer = []
+    
+    for item in arr:
+        if len(answer) == 0:
+            answer.append(item)
+        elif answer[-1] != item:
+            answer.append(item)
+    
+    return answer
