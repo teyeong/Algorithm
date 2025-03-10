635877bad4753909558034774b91fd0ab41d9ced
Taeyoung Kwon <100225783+teyeong@users.noreply.github.com>
2025-03-10 22:09:56 +0900

    [Silver I] Title: 소수&팰린드롬, Time: 3512 ms, Memory: 34536 KB -BaekjoonHub

--- 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.py
+++ 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.py
@@ -7,27 +7,11 @@ def is_prime(x):
         if x % i == 0:
             return False
     return True
-    
-def is_palindrome(x):
-    left, right = 0, 0
-    length = len(x)
-    if length % 2 == 0:
-        left, right = length // 2 - 1, length // 2
-    else:
-        left, right = length // 2 - 1, length // 2 + 1
-    
-    while left >= 0 and right < length:
-        if x[left] == x[right]:
-            left -= 1
-            right += 1
-        else:
-            return False
-    return True
 
 N = int(input())
 
 while True:
-    if is_prime(N) and is_palindrome(list(str(N))):
+    if is_prime(N) and str(N) == str(N)[::-1]:
         print(N)
         break
     else:
