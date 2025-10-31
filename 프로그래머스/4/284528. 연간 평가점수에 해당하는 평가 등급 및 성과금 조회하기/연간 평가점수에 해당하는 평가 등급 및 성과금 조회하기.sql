f3790289861e9985d2d3c051e6cea00aa369d966
Taeyoung Kwon <100225783+teyeong@users.noreply.github.com>
2025-11-01 08:20:09 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -0,0 +1,17 @@
+SELECT G.EMP_NO, E.EMP_NAME,
+       CASE 
+         WHEN AVG(G.SCORE) >= 96 THEN 'S'
+         WHEN AVG(G.SCORE) >= 90 THEN 'A'
+         WHEN AVG(G.SCORE) >= 80 THEN 'B'
+         ELSE 'C'
+       END AS GRADE,
+       CASE 
+         WHEN AVG(G.SCORE) >= 96 THEN 0.2 * E.SAL
+         WHEN AVG(G.SCORE) >= 90 THEN 0.15 * E.SAL
+         WHEN AVG(G.SCORE) >= 80 THEN 0.1 * E.SAL
+         ELSE 0
+       END AS BONUS
+FROM HR_EMPLOYEES AS E
+JOIN HR_GRADE AS G ON E.EMP_NO = G.EMP_NO
+GROUP BY G.EMP_NO
+ORDER BY G.EMP_NO;
